fec8cbce4fc6e36b056c154df920944473b32c3c
khmrh <kiedoda2@gmaiol.com>
2025-11-18 08:54:02 +0900

    [feat] 미완 10강 과제

--- GEA_137051_middle/Assets/Scrpts/s2Scripts/InventoryUI.cs
+++ GEA_137051_middle/Assets/Scrpts/s2Scripts/InventoryUI.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<Transform> slot = new List<Transform>();
+    public GameObject SLotItem;
+    List<GameObject> items = new List<GameObject>();
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateInventory(Inventory myInven)
     {
-        
+        foreach (var slotItem in items)
+        {
+            Destroy(slotItem);
+        }
+        items.Clear();
+
+        int idx = 0;
+        foreach (var item in myInven.items)
+        {
+            if (idx >= slot.Count) break;
+
+            var go = Instantiate(SLotItem, slot[idx]);
+            go.transform.localPosition = Vector3.zero;
+
+            SlotitemPreFab sItem = go.GetComponent<SlotitemPreFab>();
+            items.Add(go);
+
+            switch (item.Key)
+            {
+                case BlockType.Dirt:
+                    sItem.itemSetting(Resources.Load<Sprite>("Sprites/Items/Dirt"), item.Value.ToString());
+                    break;
+                case BlockType.Grass:
+                    sItem.itemSetting(Resources.Load<Sprite>("Sprites/Items/Grass"), item.Value.ToString());
+                    break;
+                case BlockType.Water:
+                    sItem.itemSetting(Resources.Load<Sprite>("Sprites/Items/Water"), item.Value.ToString());
+                    break;
+                default:
+                    break;
+            }
+            idx++;
+        }
     }
 }
